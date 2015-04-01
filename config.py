f4b7bd57eacf31d149425a679cbb4050a692e033
Bobby <stevens4@ph.utexas.edu>
2015-04-01 13:22:05 -0500

    initialized project. starting with a (mostly) working GUI. no device functionality working yet.

---
+++ config.py
@@ -0,0 +1,172 @@
+'''
+by stevens4
+
+a configuration dictionary for the Rover3 project
+
+just as in sitzlabexpcontrol, the configuration parameters for each 
+channel are stored in a dictionary of dictionaries
+
+
+'''
+import os
+roverLogPath = os.path.relpath('logs')
+
+poohAIColumn = 2
+tiggerAIColumn = 7
+
+analogInputsConfDict = {
+    'water temperature': {
+		'guiRow': 0,
+		'guiColumn':3,
+		'labelText': 'cooling water',
+		'chamber':'both',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	},	
+	
+    'pooh main foreline pressure': {
+		'guiRow': 3,
+		'guiColumn':poohAIColumn,
+		'labelText': 'main foreline',
+		'chamber':'pooh',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	},
+	
+    'pooh source foreline pressure': {
+		'guiRow': 2,
+		'guiColumn':poohAIColumn,
+		'labelText': 'source foreline',
+		'chamber':'pooh',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	},
+	
+	'tigger source foreline pressure': {
+		'guiRow': 2,
+		'guiColumn':tiggerAIColumn,
+		'labelText': 'source foreline',
+		'chamber':'tigger',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	},
+	
+	'tigger buffer foreline pressure': {
+		'guiRow': 3,
+		'guiColumn':tiggerAIColumn,
+		'labelText': 'buffer foreline',
+		'chamber':'tigger',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	},
+	
+    'tigger main foreline pressure': {
+		'guiRow': 4,
+		'guiColumn':tiggerAIColumn,
+		'labelText': 'main foreline',
+		'chamber':'tigger',
+		'physicalChannel':'chan1',
+		'scaling':1,
+		'offset':0
+	}
+}
+
+
+poohControlsColumn = 0
+tiggerControlsColumn = 6 
+
+digitalOutputsConfDict = {
+    'pooh source/buffer cooling water': {
+		'guiRow': 6,
+		'guiColumn':poohControlsColumn,
+		'labelText': 'source/buffer cooling water',
+		'chamber':'pooh',
+		'physicalChannel':'chan1'
+	},	
+	
+    'pooh source roughing pump': {
+		'guiRow': 7,
+		'guiColumn':poohControlsColumn,
+		'labelText': 'source roughing pump',
+		'chamber':'pooh',
+		'physicalChannel':'chan1'
+	},
+	
+    'pooh main roughing': {
+		'guiRow': 8,
+		'guiColumn':poohControlsColumn,
+		'labelText': 'main roughing pump',
+		'chamber':'pooh',
+		'physicalChannel':'chan1'
+	},
+	
+    'pooh main gate valve': {
+		'guiRow': 9,
+		'guiColumn':poohControlsColumn,
+		'labelText': 'main gate valve',
+		'chamber':'pooh',
+		'physicalChannel':'chan1'
+	},
+
+    'pooh main foreline valve': {
+		'guiRow': 10,
+		'guiColumn':poohControlsColumn,
+		'labelText': 'main foreline valve',
+		'chamber':'pooh',
+		'physicalChannel':'chan1'
+	},
+
+	'tigger source foreline valve': {
+		'guiRow': 6,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'source foreline valve',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	},
+	
+	'tigger main gate valve': {
+		'guiRow': 7,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'main gate valve',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	},
+	
+    'tigger diffusion pumps': {
+		'guiRow': 8,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'diffusion pumps',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	},
+
+	'tigger main foreline valve': {
+		'guiRow': 9,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'main foreline valve',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	},
+
+	'tigger detector gauge': {
+		'guiRow': 10,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'main detector gauge',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	},
+
+	'tigger water valve': {
+		'guiRow': 11,
+		'guiColumn':tiggerControlsColumn,
+		'labelText': 'water valve',
+		'chamber':'tigger',
+		'physicalChannel':'chan1'
+	}
+
+}
---
+++ lowLevelLibrary.py
@@ -0,0 +1,137 @@
+
+# create AI channel objects
+class aiChannel:
+	def __init__(self,confDict):
+		#open connection on physicalChannel
+		self.name = confDict['name']
+		self.physChan = confDict['physicalChannel']
+		self.scaling = confDict['scaling']
+		self.offset = confDict['offset']
+	
+	def _map(self,rawReading):
+		mappedVoltage = self.scaling*rawReading + self.offset
+		return mappedVoltage
+		
+	def getReading(self):
+		#measure latest
+		return reading
+	
+	def getNReadings(self,numToRead):
+		readings = []
+		while len(readings) < numToRead:
+			newReading = self.getReading()
+			readings.append(newReading)
+		return readings
+		
+
+
+# create DO channel objects
+class doChannel:
+	def __init__(self,confDict):
+		#open connection on physicalChannel
+		self.name = confDict['name']
+		self.physChan = confDict['physicalChannel']
+		self.interlocks = {}
+		
+	def setState(self, newState):
+		#self.physChan.write(newState)
+		print 'state on '+self.name+' has been changed to: '+str(newState)
+		
+	def getState(self):
+		#state = self.physChan.read()
+		return state
+		
+	def createInterlock(self,aiChan,limitVal,logicalFunction):
+		newInterlock = interlock(aiChan,limitVal,logicalFunction)
+		interlockIndex = len(self.interlocks.keys())
+		self.interlocks[interlockIndex] = newInterlock
+		
+	def addInterlock(self,interlock):
+		interlockIndex = len(self.interlocks.keys())
+		self.interlocks[interlockIndex] = interlock
+		
+	def deleteInterlock(self,interlockKey):
+		del self.interlocks[interlockKey]
+		
+	def toggleInterlock(self,interlock,newState):
+		self.interlocks[interlock].setState(newState)
+
+	def getInterlocks(self):
+		return self.interlocks
+		
+	def testInterlocks(self):
+		for interlock in self.interlocks.values():
+			if interlock.testInterlock() and interlock.getState():
+				print 'interlock tripped on '+self.name
+				print str(interlock.aiChannel)+' registered above setpoint: '+str(interlock.getLimitValue())
+				self.setState(False)
+
+
+
+
+# create interlock object
+# upon initialization it takes an analog input channel (e.g. water temp)
+# to monitor, a limit value (e.g. 30 degrees). These will not be directly
+# initialized but rather created/disabled/destroyed by a digital channel
+# object which the interlock can toggle when limit is crossed as per 
+# logicalFunction.
+
+
+# interlocks will be test of the logical form:
+# if [AI_CHANNEL] is [LOGICAL_FUNCTION] [LIMITVAL] then turn off
+
+
+import operator
+LOGICAL_FUNCTIONS = {
+	'>': operator.gt,
+	'>=': operator.ge,
+	'<': operator.lt,
+	'<=': operator.le,
+	'==': operator.eq,
+	'!=': operator.ne
+}
+
+class interlock:
+		def __init__(self,aiChan,limitVal,logicalFunctionStr):
+			self.aiChannel = aiChan
+			self.limitValue = limitVal
+			self.logicalFunction = logicalFunctionStr
+			self.enabled = True
+		
+		def setState(self,newState):
+			self.enabled = newState
+			
+		def getState(self):
+			return self.enabled
+			
+		def setSenseChannel(self,aiChan):
+			self.aiChannel = aiChan
+			
+		def getSenseChannel(self):
+			return self.aiChannel  #.name
+			
+		def setLimitValue(self,limitVal):
+			self.limitValue = limitVal
+			
+		def getLimitValue(self):
+			return self.limitValue
+			
+		def setLogicalFunction(self,logFunc):
+			self.logicalFunction = logFunc
+			
+		def getLogicalFunction(self):
+			return self.logicalFunction
+			
+		def testInterlock(self):
+			function = LOGICAL_FUNCTIONS[self.logicalFunctionStr]
+			latestReading = self.aiChannel.getReading()
+			interlockTripped = function(latestReading,self.limitValue)
+			return interlockTripped
+			
+		
+
+
+
+
+
+
